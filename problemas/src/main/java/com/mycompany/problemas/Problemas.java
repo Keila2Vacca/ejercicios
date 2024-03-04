2d82d9439e1eacd4390974dab0cce261cd40356c
Keila2Vacca <knvaccab@ufpso.edu.co>
2024-03-04 10:47:03 -0500

    inicializando

---
+++ problemas/src/main/java/com/mycompany/problemas/Problemas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.problemas;
+
+/**
+ *
+ * @author SCIS2PC24
+ */
+public class Problemas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
